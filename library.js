a353cbf71459f9ef7e084caec34f087ae689f870
Austin Keller <austinkllr@gmail.com>
2021-12-18 23:33:30 -0800

    finished delete book functionality

--- library.js
+++ library.js
@@ -16,44 +16,70 @@ function Book(title, author, pages, read) {
     }
 }
 
+displayBooks();
+
+
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
 function displayBooks() {
     let booksContainer = document.querySelector('.books');
+    
+
+
+   while (booksContainer.firstChild) {
+       booksContainer.removeChild(booksContainer.firstChild);
+   }
 
     for (let i = 0; i < myLibrary.length; i++) {
-        booksContainer.appendChild(createBookCard(myLibrary[i]));
+        let newBook = createBookCard(myLibrary[i], i)
+        
+        let removeBtn = document.createElement('button');
+        removeBtn.setAttribute('id', 'remove-book');
+        removeBtn.textContent = "Remove book";
+
+        newBook.appendChild(removeBtn);
+
+        booksContainer.appendChild(newBook);
     }
+
+    let removeBookBtns = document.querySelectorAll('#remove-book');
+    removeBookBtns.forEach(btn => btn.addEventListener('click', removeBook));
+
 }
 
-function createBookCard(book) {
+function createBookCard(book, index) {
     let container = document.createElement('div');
+    container.setAttribute('id', `${index}`);
 
     let title = document.createElement('h3');
     let author = document.createElement('p');
     let pages = document.createElement('p');
     let read = document.createElement('p');
 
+    
+
     title.textContent = book.title;
     author.textContent = book.author;
     pages.textContent = `Pages: ${book.pages}`;
     read.textContent = `Read: ${book.read}`;
 
-    container.appendChild(title);
-    container.appendChild(author);
-    container.appendChild(pages);
-    container.appendChild(read);
+    container.append(title, author, pages, read);
 
     return container;
 }
+
 
 let newBookBtn = document.querySelector('#new-book');
 let newBookForm = document.querySelector('form');
 let addBookBtn = document.querySelector("#add-book");
 
+
 function displayForm() {
+    // clears form before each use
+    newBookForm.reset();                                 
+
     newBookForm.hidden = false;
 }
 
@@ -73,8 +99,21 @@ function addBookFromForm() {
     
     addBookToLibrary(book);
     hideForm();
+    displayBooks();
     
 }
 
+function removeBook(e) {
+    myLibrary.splice(e.target.parentNode.id, 1);
+    e.target.parentNode.remove(); 
+
+    displayBooks();
+
+}
+
+
+
+
 newBookBtn.addEventListener('click', displayForm);
 addBookBtn.addEventListener('click', addBookFromForm);
+
